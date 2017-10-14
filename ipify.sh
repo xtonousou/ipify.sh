acddbe964a54ff232a77b811f46a468e470ff126
xtonousou <xtonousou@gmail.com>
2017-10-14 15:01:39 +0300

    Fixed some 'things'

--- ipify.sh
+++ ipify.sh
@@ -4,54 +4,64 @@
 # Exit Values ..: {"0": "successful", "1": "arg issues", "2": "connection issues"}
 # Dependencies .: bash>=3.2, coreutils, curl
 # Linter .......: shellcheck
-# Date .........: Thu Aug 31 09:57:41 EEST 2017
+# Date .........: Sat Oct 14 14:55:37 EEST 2017
 
-function get_ip() {
-  function help() {
-    printf "%s%16s%8s\n%s%7s%19s\n%s%7s%16s\n%s%7s%25s\n%s%7s%35s\n%s%6s%32s" \
+get_ip() {
+
+  declare PRINTF
+
+  PRINTF=$(which printf)
+
+  help() {
+    "${PRINTF}" \
+      "%s%16s%8s\n%s%7s%19s\n%s%7s%16s\n%s%7s%25s\n%s%7s%35s\n%s%6s%32s\n" \
       "COMMAND" "TYPE" "OUTPUT" \
-      "ipify -h, --help" "text" "This help message" \
-      "ipify -t, --text" "text" "98.207.254.136" \
-      "ipify -j, --json" "json" "{\"ip\":\"98.207.254.136\"}" \
-      "ipify -J, --jsonp" "jsonp" "callback({\"ip\":\"98.207.254.136\"});" \
-      "ipify -G, --get-ip" "jsonp" "getip({\"ip\":\"98.207.254.136\"});"
+      "get_ip -h, --help" "text" "This help message" \
+      "get_ip -t, --text" "text" "98.207.254.136" \
+      "get_ip -j, --json" "json" "{\"ip\":\"98.207.254.136\"}" \
+      "get_ip -J, --jsonp" "jsonp" "callback({\"ip\":\"98.207.254.136\"});" \
+      "get_ip -g, --get-ip" "jsonp" "getip({\"ip\":\"98.207.254.136\"});"
 
     return 0
   }
   
   [ "${#}" -eq 0 ] && help && return 1
-  
+
+  declare CURL
   declare API_URI
   declare RESPONSE
 
+  CURL=$(which curl)
+  API_URI="https://api.ipify.org"
+
   case "${1}" in
-    "-h"|"--help")
-      help && return 0
+  "-h"|"--help")
+    help && return 0
     ;;
-    "-t"|"--text")
-      API_URI="https://api.ipify.org"
+  "-t"|"--text")
     ;;
-    "-j"|"--json")
-      API_URI="https://api.ipify.org?format=json"
+  "-j"|"--json")
+    API_URI+="?format=json"
     ;;
-    "-J"|"--jsonp")
-      API_URI="https://api.ipify.org?format=jsonp"
+  "-J"|"--jsonp")
+    API_URI+="?format=jsonp"
     ;;
-    "-G"|"--get-ip")
-      API_URI="https://api.ipify.org?format=jsonp&callback=getip"
+  "-g"|"--get-ip")
+    API_URI+="?format=jsonp&callback=getip"
     ;;
-    *)
-      help && return 1
+  *)
+    help && return 1
     ;;
   esac
 
-  if ! RESPONSE=$($(which curl) --disable --fail --silent "${API_URI}")
-  then
-    printf "ipify: check your internet connection" && return 2
+  if ! RESPONSE=$("${CURL}" --disable --fail --silent "${API_URI}"); then
+    "${PRINTF}" "ipify: check your internet connection" && return 2
   fi
 
-  printf "%s" "${RESPONSE}"
+  "${PRINTF}" "%s" "${RESPONSE}"
 
+  unset PRINTF
+  unset CURL
   unset API_URI
   unset RESPONSE
 
